753008c2241c82e1f43e4b3ccababb9b462ed808
Carlos Henrique <carloshenriquemg2020@gmail.com>
2022-11-18 15:58:41 -0300

    update-delete-user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,37 @@
+const { restart } = require("nodemon");
 var User = require("../models/User");
 class UserController {
+  // CREATE USER
+  async create(req, res) {
+    var { email, name, password } = req.body;
+
+    if (email == undefined) {
+      res.status(400);
+      res.json({ error: "O e-mail é invalido!" });
+      return;
+    }
+
+    var emailExist = await User.findEmail(email); // EMAIL VERIFICATION
+
+    if (emailExist) {
+      res.status(406);
+      res.json({ err: "o e-mail já está cadastrado!" });
+      return;
+    }
+
+    await User.new(email, password, name);
+
+    res.status(200);
+    res.send("Tudo ok!");
+  }
+
+  // GETTING ALL USERS
   async index(req, res) {
     var users = await User.findAll();
     res.json(users);
   }
 
+  // GETTING A SPECIFIC USER
   async findUser(req, res) {
     var id = req.params.id;
     var user = await User.findById(id);
@@ -18,27 +45,38 @@ class UserController {
     }
   }
 
-  async create(req, res) {
-    var { email, name, password } = req.body;
+  // UPDATING USER
+  async edit(req, res) {
+    var { id, name, email, role } = req.body;
+    var result = await User.update(id, name, email, role);
 
-    if (email == undefined) {
-      res.status(400);
-      res.json({ error: "O e-mail é invalido!" });
-      return;
-    }
-
-    var emailExist = await User.findEmail(email);
-
-    if (emailExist) {
+    if (result != undefined) {
+      if (result.status) {
+        res.status(200);
+        res.send("Tudo certo!");
+      } else {
+        res.status(406);
+        res.send(result.err);
+      }
+    } else {
       res.status(406);
-      res.json({ err: "o e-mail já está cadastrado!" });
-      return;
+      res.send("Ocorreu um erro no servidor");
     }
+  }
 
-    await User.new(email, password, name);
+  // DELETE USER
+  async remove(req, res) {
+    var id = req.params.id;
 
-    res.status(200);
-    res.send("Tudo ok!");
+    var result = await User.delete(id);
+
+    if (result.status) {
+      res.status(200);
+      res.send("Apagado!");
+    } else {
+      res.status(406);
+      res.send(result.err);
+    }
   }
 }
 
--- models/User.js
+++ models/User.js
@@ -2,6 +2,29 @@ var knex = require("../database/connection");
 var bcrypt = require("bcrypt");
 const { ServerHandshake } = require("mysql2/lib/commands");
 class User {
+  // CREATE USER
+  async new(email, password, name) {
+    try {
+      var hash = await bcrypt.hash(password, 10);
+
+      await knex
+        .insert({ email, password: hash, name, role: 0 })
+        .table("users");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  // EMAIL VERIFICATION
+  async findEmail(email) {
+    try {
+      var result = await knex.select("*").from("users").where({ email: email });
+      if (result.length > 0) return true;
+      else return false;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  // GETTING ALL USERS
   async findAll() {
     try {
       var result = await knex
@@ -13,7 +36,7 @@ class User {
       return [];
     }
   }
-
+  // GETTING A SPECIFIC USER
   async findById(id) {
     try {
       var result = await knex
@@ -31,26 +54,61 @@ class User {
       return undefined;
     }
   }
+  // UPDATING USER
+  async update(id, name, email, role) {
+    var user = await this.findById(id);
 
-  async new(email, password, name) {
-    try {
-      var hash = await bcrypt.hash(password, 10);
+    if (user != undefined) {
+      var editUser = {};
 
-      await knex
-        .insert({ email, password: hash, name, role: 0 })
-        .table("users");
-    } catch (err) {
-      console.log(err);
+      if (email != undefined) {
+        if (email != user.email) {
+          var result = await this.findEmail(email);
+          if (result == false) {
+            editUser.email = email;
+          } else {
+            return { status: false, err: "O email já está cadastrado!" };
+          }
+        }
+      }
+
+      if (name != undefined) {
+        editUser.name = name;
+      }
+
+      if (role != undefined) {
+        editUser.role = role;
+      }
+      //ATUALIZANDO DADOS
+      try {
+        await knex.update(editUser).where({ idusers: id }).table("users");
+        return { status: true };
+      } catch (err) {
+        return { status: false, err: err };
+      }
+    } else {
+      return { status: false, err: "O usuário não existe!" };
     }
   }
+  // DELETE USER
+  async delete(id) {
+    var user = await this.findById(id);
 
-  async findEmail(email) {
-    try {
-      var result = await knex.select("*").from("users").where({ email: email });
-      if (result.length > 0) return true;
-      else return false;
-    } catch (err) {
-      console.log(err);
+    if (user != undefined) {
+      try {
+        await knex.delete().where({ idusers: id }).table("users");
+        return { status: true };
+      } catch (err) {
+        return {
+          status: false,
+          err: err,
+        };
+      }
+    } else {
+      return {
+        status: false,
+        err: "O usuário não existe, portanto não pode ser deletado!",
+      };
     }
   }
 }
